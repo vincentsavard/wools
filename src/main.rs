a91064c0cafdf24c0a04df9684f6915ad0e2aeef
Vincent Savard <savard.vincent@gmail.com>
2022-05-07 09:39:39 -0400

    feat: open Wordle in the default browser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::ffi::{OsStr, OsString};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
@@ -8,6 +9,7 @@ use clap::{Parser, Subcommand};
 use wools::Word;
 
 const DICTIONARY_FILE_PATH: &str = "/usr/share/dict/american-english";
+const DEFAULT_WORDLE_URL: &str = "https://www.nytimes.com/games/wordle/index.html";
 
 #[derive(Parser)]
 #[clap(version, about)]
@@ -33,6 +35,11 @@ enum Command {
     },
     /// Displays the list of valid, normalized words from the dictionary.
     Dict,
+    /// Opens Wordle in the default browser.
+    Open {
+        #[clap(short, long, default_value = DEFAULT_WORDLE_URL)]
+        url: OsString,
+    },
 }
 
 fn main() -> Result<(), String> {
@@ -42,6 +49,7 @@ fn main() -> Result<(), String> {
     match opt.command {
         Command::Filter { solution, guesses } => filter(words, solution, guesses),
         Command::Dict => dict(words),
+        Command::Open { url } => open(url),
     }
 }
 
@@ -70,4 +78,15 @@ fn dict(words: Vec<Word>) -> Result<(), String> {
     }
 
     Ok(())
+}
+
+fn open<S: AsRef<OsStr>>(url: S) -> Result<(), String> {
+    let output = std::process::Command::new("xdg-open").arg(url).output();
+
+    match output {
+        Ok(output) if output.status.success() => Ok(()),
+        Ok(output) if output.stderr.is_empty() => Err(output.status.to_string()),
+        Ok(output) => Err(format!("{}", String::from_utf8_lossy(&output.stderr))),
+        Err(error) => Err(error.to_string()),
+    }
 }
